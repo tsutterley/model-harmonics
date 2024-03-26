14c4492f8f6c708cce812d33c37f959e03dee8a7
tsutterley <tsutterl@uw.edu>
2024-03-26 16:42:32 -0700

    Update test_ellipsoid.py

--- test/test_ellipsoid.py
+++ test/test_ellipsoid.py
@@ -14,28 +14,28 @@ def test_ellipsoid(ellipsoid, units):
     # get dictionary output from geoid-toolkit
     ellipsoid_params = geoidtk.ref_ellipsoid(ellipsoid, UNITS=units)
     # get class output from model-haromnics
-    refell = mdlhmc.datum(ellipsoid=ellipsoid, units=units)
+    units = mdlhmc.datum(ellipsoid=ellipsoid, units=units)
     # assert values
-    assert ellipsoid_params['a'] == refell.a_axis
-    assert ellipsoid_params['b'] == refell.b_axis
-    assert ellipsoid_params['f'] == refell.flat
-    assert ellipsoid_params['rad_e'] == refell.rad_e
-    assert ellipsoid_params['rad_p'] == refell.rad_p
-    assert ellipsoid_params['ratio'] == refell.ratio
-    assert ellipsoid_params['GM'] == refell.GM
-    assert ellipsoid_params['omega'] == refell.omega
-    assert ellipsoid_params['C20'] == refell.C20
-    assert ellipsoid_params['J2'] == refell.J2
-    assert ellipsoid_params['U0'] == refell.U0
-    assert ellipsoid_params['dk'] == refell.dk
-    assert ellipsoid_params['norm_a'] == refell.ga
-    assert ellipsoid_params['norm_b'] == refell.gb
-    assert ellipsoid_params['mp'] == refell.m
-    assert ellipsoid_params['q'] == refell.q
-    assert ellipsoid_params['q0'] == refell.q0
-    assert ellipsoid_params['ecc'] == refell.ecc
-    assert ellipsoid_params['ecc1'] == refell.ecc1
-    assert ellipsoid_params['ecc2'] == refell.ecc2
-    assert ellipsoid_params['area'] == refell.area
-    assert ellipsoid_params['volume'] == refell.volume
-    assert ellipsoid_params['rho_e'] == refell.rho_e
+    assert ellipsoid_params['a'] == units.a_axis
+    assert ellipsoid_params['b'] == units.b_axis
+    assert ellipsoid_params['f'] == units.flat
+    assert ellipsoid_params['rad_e'] == units.rad_e
+    assert ellipsoid_params['rad_p'] == units.rad_p
+    assert ellipsoid_params['ratio'] == units.ratio
+    assert ellipsoid_params['GM'] == units.GM
+    assert ellipsoid_params['omega'] == units.omega
+    assert ellipsoid_params['C20'] == units.C20
+    assert ellipsoid_params['J2'] == units.J2
+    assert ellipsoid_params['U0'] == units.U0
+    assert ellipsoid_params['dk'] == units.dk
+    assert ellipsoid_params['norm_a'] == units.ga
+    assert ellipsoid_params['norm_b'] == units.gb
+    assert ellipsoid_params['mp'] == units.m
+    assert ellipsoid_params['q'] == units.q
+    assert ellipsoid_params['q0'] == units.q0
+    assert ellipsoid_params['ecc'] == units.ecc
+    assert ellipsoid_params['ecc1'] == units.ecc1
+    assert ellipsoid_params['ecc2'] == units.ecc2
+    assert ellipsoid_params['area'] == units.area
+    assert ellipsoid_params['volume'] == units.volume
+    assert ellipsoid_params['rho_e'] == units.rho_e
