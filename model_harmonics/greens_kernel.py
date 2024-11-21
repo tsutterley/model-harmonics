40b0f2eaf7ae6b1f26cc5b66579f92b3b2b98acd
tsutterley <tsutterl@uw.edu>
2024-11-21 13:38:25 -0800

    Update greens_kernel.py

--- model_harmonics/greens_kernel.py
+++ model_harmonics/greens_kernel.py
@@ -19,9 +19,9 @@ OPTIONS:
     LOVE: List of Love Numbers kl, hl, and ll
 
 OUTPUTS:
-    X: X-coordinates of the kernel
-    Y: Y-coordinates of the kernel
-    G: Green's function kernel
+    X: X-coordinates of the kernel (meters)
+    Y: Y-coordinates of the kernel (meters)
+    G: Green's function kernel (m/kg)
 
 PYTHON DEPENDENCIES:
     numpy: Scientific Computing Tools For Python (https://numpy.org)
@@ -85,8 +85,8 @@ def greens_kernel(LMAX, SPACING=[], WIDTH=[], CUTOFF=0.0, LOVE=None):
     assert np.isclose(scale, 6.371e6/5.972e24)
     # extract arrays of kl, hl, and ll Love Numbers
     hl, kl, ll = LOVE
-    # scale to unnormalize Legendre polynomials
-    norm = 1.0/np.sqrt(2.0*np.arange(LMAX+1) + 1)
+    # scale used to originally normalize the Legendre polynomials
+    norm = np.sqrt(2.0*np.arange(LMAX+1) + 1)
     # grid spacing
     dx,dy = np.broadcast_to(np.atleast_1d(SPACING),(2,))
     # grid width
@@ -112,25 +112,24 @@ def greens_kernel(LMAX, SPACING=[], WIDTH=[], CUTOFF=0.0, LOVE=None):
                 Pl = np.zeros((LMAX+1))
                 # divide pixel area by area of a half sphere
                 alpha = (1.0 - (dx*dy)/(2.0*np.pi*rad_e**2))
-                # l=0 is a special case (P(-1) = 1, P(1) = cos(alpha))
+                # l=0 is a special case
                 Pl[0] = (1.0 - alpha)/2.0
                 # Legendre polynomials up to LMAX+1
                 Ptemp,_ = gravtk.legendre_polynomials(LMAX+1, alpha)
-                for l in range(1, LMAX+1):# LMAX+1 to include LMAX
-                    # from Longman (1962) and Jacob et al (2012)
+                for l in range(1, LMAX+1):
                     # unnormalizing Legendre polynomials
                     # sqrt(2*l - 1) == sqrt(2*(l-1) + 1)
                     # sqrt(2*l + 3) == sqrt(2*(l+1) + 1)
-                    pl_lower = Ptemp[l-1]/np.sqrt(2.0*l-1.0)
-                    pl_upper = Ptemp[l+1]/np.sqrt(2.0*l+3.0)
-                    Pl[l] = (pl_lower - pl_upper)/2.0
+                    pll = Ptemp[l-1]/np.sqrt(2.0*l - 1.0)
+                    plu = Ptemp[l+1]/np.sqrt(2.0*l + 3.0)
+                    Pl[l] = (pll - plu)/2.0
             else:
-                # Legendre polynomials up to LMAX+1
+                # Legendre polynomials up to LMAX
                 alpha = np.cos(D[j,i]/rad_e)
                 Ptemp,_ = gravtk.legendre_polynomials(LMAX, alpha)
                 # unnormalizing Legendre polynomials
-                Pl = norm*np.squeeze(Ptemp)
+                Pl = np.squeeze(Ptemp)/norm
             # calculate Green's function
             G[j,i] = scale*np.sum(hl*Pl)
-    # return the Green's function
+    # return the Green's function and the coordinates
     return (X, Y, G)
